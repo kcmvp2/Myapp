53bc77f488b590cde1965165bdb5d6141b2cdd7e
kcmvp2 <kcmvp2@gmail.com>
2023-01-08 22:42:10 +0000

    Revert "added e in the code"
    This reverts commit df5a1f02586c82a8f71c824c75f66de86667dbfb.

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 # This is a comment in python
 
+<<<<<<< HEAD
+
+a=3128
+=======
 a=4128
+>>>>>>> 4c69619 (second commit from main branch)
 
 b=4289
 
@@ -8,10 +13,13 @@ c=4318
 
 d=4499
 
+<<<<<<< HEAD
 e=6677
 
 f=4455
 
+=======
+>>>>>>> parent of df5a1f0 (added e in the code)
 sum = a+b+c+d
 
 print ("output of two numbers is = ", sum )
